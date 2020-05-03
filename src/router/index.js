2f9b2be65a61dbd308039e84d339e048bb87b585
xz <12345@qq.com>
2020-05-03 09:18:02 +0800

    完成了登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login'
+import Home from '../components/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: './login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
 })
-
+// 为路由对象,添加路由导航守卫
+router.beforeEach((to, from, next) => {
+  // 如果用户访问登录页,直接放行.如果访问的有权限的页面,则从sessionStorage中获取token并判断.
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
